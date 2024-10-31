3aacec14301fe03691659d53a7d53a7716c2cd05
Martin Bohorquez <121200468+martinbohorquez@users.noreply.github.com>
2024-10-31 00:35:15 -0500

    Agregar dark/light theme, imagenes y estidos

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,70 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { CartItem } from '../models/cartItem';
 import { Product } from '../models/product';
+import { CartItemService } from '../services/cart-item.service';
 import { ProductService } from '../services/product.service';
 import { CartComponent } from './cart/cart.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
-  selector: 'cart-app',
-  standalone: true,
-  imports: [CatalogComponent, CartComponent, NavbarComponent],
-  templateUrl: './cart-app.component.html',
-  styleUrl: './cart-app.component.css'
+	selector: 'cart-app',
+	standalone: true,
+	imports: [CatalogComponent, CartComponent, NavbarComponent],
+	templateUrl: './cart-app.component.html',
+	styleUrl: './cart-app.component.css'
 })
 export class CartAppComponent implements OnInit {
+	products: Product[] = [];
+	items: CartItem[] = [];
+	total: number = 0;
+	showCart: boolean = false;
 
-  products: Product[] = [];
-  items: CartItem[] = [];
-  total: number = 0;
-  showCart: boolean = false;
+	constructor(private productService: ProductService, private cartItemService: CartItemService) {}
 
-  constructor(private productService: ProductService) { }
+	ngOnInit(): void {
+		this.products = this.productService.findAll();
+		this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
+		this.calculateTotal();
+	}
 
-  ngOnInit(): void {
-    this.products = this.productService.findAll();
-    this.items = JSON.parse(sessionStorage.getItem('cart')  || '[]');
-    this.total = this.calculateTotal();
-  }
+	onAddCart(product: Product): void {
+		this.items = this.cartItemService.addProduct(this.items, product);
+		this.calculateTotal();
+		this.saveSession();
+	}
 
-  onAddCart(product: Product): void {
-    const hasItem = this.items.find(item => item.product.id === product.id);
-    if (hasItem) {
-      this.items = this.items.map(item => {
-        if (item.product.id === product.id) {
-          return { ...item, quantity: item.quantity + 1 };
-        }
-        return item;
-      })
-    } else {
-      this.items = [... this.items, { product: { ...product }, quantity: 1 }];
-    }
-    this.setTotalAndSaveSession();
-  }
+	onDeleteCart(id: number): void {
+		this.items = this.cartItemService.removeProduct(this.items, id);
+		this.calculateTotal();
+		this.saveSession();
+		if (this.items.length == 0) {
+			sessionStorage.removeItem('cart');
+		}
+	}
 
-  onDeleteCart(id: number): void {
-    this.items = this.items.filter(item => item.product.id != id);
-    this.setTotalAndSaveSession();
-  }
+	saveSession(): void {
+		sessionStorage.setItem('cart', JSON.stringify(this.items));
+	}
 
-  calculateTotal(): number {
-    return this.items.reduce((accumulator, item) => {
-      return accumulator + (item.quantity * item.product.price);
-    }, 0);
-  }
+	openCloseCart(): void {
+		this.showCart = !this.showCart;
+	}
 
-  saveSession(): void {
-    sessionStorage.setItem('cart', JSON.stringify(this.items));
-  }
-
-  setTotalAndSaveSession():void {
-    this.total = this.calculateTotal();
-    this.saveSession();
-  }
-
-  openCart(): void {
-    this.showCart = !this.showCart;
-  }
-
+	calculateTotal(): void {
+		this.total = this.items.reduce((accumulator, item) => {
+			return accumulator + item.quantity * item.product.price;
+		}, 0);
+	}
 }
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,6 +1,7 @@
 export class Product {
-    id: number = 0;
-    name: string = '';
-    description: string = '';
-    price: number = 0;
+	id: number = 0;
+	name: string = '';
+	description: string = '';
+	imageUrl: string = '';
+	price: number = 0;
 }
